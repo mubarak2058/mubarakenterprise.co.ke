012f5424f950a7fbfd6215ffe0002e160917e9e5
@mubarakbizna <mubarakjumamasoud@gmail.com>
2026-06-28 12:43:41 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
+// Firebase v11
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBMO6BGkLT6LNYLZJbmghmNeOmgqzquXjo",
+  authDomain: "mubarak-enterprise-pos.firebaseapp.com",
+  projectId: "mubarak-enterprise-pos",
+  storageBucket: "mubarak-enterprise-pos.firebasestorage.app",
+  messagingSenderId: "631510451226",
+  appId: "1:631510451226:web:1c36b45bdfb2bf9538fda3",
+  measurementId: "G-14FXK18W73"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
